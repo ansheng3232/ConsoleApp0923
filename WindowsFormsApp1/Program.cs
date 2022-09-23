52b73ba7997121bd267cf9f2c8a840fc94b7d87f
ansheng3232 <87800345+ansheng3232@users.noreply.github.com>
2022-09-23 14:25:17 +0800

    Update Program.cs

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -13,7 +13,7 @@
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {//123
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
