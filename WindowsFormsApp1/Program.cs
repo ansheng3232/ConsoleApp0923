7cd2c50e56ace295bb4878bfd76fd145eff4f160
Chen An <v-chenan1@microsoft.com>
2022-09-22 23:29:35 -0700

    f

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -17,7 +17,6 @@
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //123
         }
-    }
+    }//456
 }
